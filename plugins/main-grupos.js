9fcf10ccecacae38041b499ceea5734c0f9cb1d3
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 18:10:10 -0500

    m

---
+++ plugins/main-grupos.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch'
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let img = await (await fetch(`https://i.ibb.co/3N4StyG/file.jpg`)).buffer()
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+let txt = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para convivir con la comunidad :D*
+
+1- 【 ✯ Starlights Team ✰ 】
+*✰* ${global.group}
+
+2- 【 ✯ Starlights Team ✰ 】- ll
+*✰* ${global.group2}
+
+3- 【 ✯ Starlights Team ✰ 】- lll
+*✰* ${global.group3}
+
+*─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ*
+
+➠ Enlaces anulados? entre aquí! 
+
+Canal :
+*✰* ${global.canal}
+
+> [ ✰ ] ${global.textbot}`
+await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
+}
+handler.help = ['grupos']
+handler.tags = ['main']
+handler.command = /^(grupos)$/i
+export default handler 
