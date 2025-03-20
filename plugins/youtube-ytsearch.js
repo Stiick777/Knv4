1f2ccba2b1d0f21c89e15c1c3abb723520932a6d
Stiick777 <kanixxxcexx@gmail.com>
2025-03-20 02:04:43 -0500

    Update youtube-ytsearch.js

--- plugins/youtube-ytsearch.js
+++ plugins/youtube-ytsearch.js
@@ -1,40 +1,44 @@
-import yts from 'yt-search';
-
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-    if (!text) throw `✳️ Ejemplo: *${usedPrefix + command}* Lil Peep hate my life`;
-    m.react('📀');
-
-    let result = await yts(text);
-    let ytres = result.videos;
-
-    let listSections = [];
-    for (let index in ytres) {
-        let v = ytres[index];
-        listSections.push({
-            title: `${index}┃ ${v.title}`,
-            rows: [
-                {
-                    header: '🎶 MP3',
-                    title: v.title,
-                    description: `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título:* ${v.title}\n▢ 📆 *Publicado:* ${v.ago}`,
-                    id: `${usedPrefix}yta ${v.url}` // Comando directo para audio
-                },
-                {
-                    header: "🎥 MP4",
-                    title: v.title,
-                    description: `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título:* ${v.title}\n▢ 📆 *Publicado:* ${v.ago}`,
-                    id: `${usedPrefix}ytv ${v.url}` // Comando directo para video
-                }
-            ]
-        });
-    }
-
-    await conn.sendList(m.chat, '≡ *YT-SEARCH MUSIC* 🔎', `📀 Resultados de: *${text}*\n\nKanBot by Stiiven`, 'Selecciona una opción', ytres[0].image, listSections, m);
-};
-
-handler.help = ['yts'];
-handler.tags = ['search'];
-handler.command = ['yts', 'ytsearch'];
+import yts from 'yt-search';  
+  
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {  
+    if (!text) throw `✳️ Ejemplo: *${usedPrefix + command}* Lil Peep hate my life`;  
+    m.react('📀');  
+      
+    let result = await yts(text);  
+    let ytres = result.videos;  
+      
+    let listSections = [];  
+    for (let index in ytres) {  
+        let v = ytres[index];  
+        listSections.push({  
+            title: `${index}┃ ${v.title}`,  
+            rows: [  
+                {  
+                    header: '🎶 MP3',  
+                    title: "",  
+                    description: `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título:* ${v.title}\n▢ 📆 *Publicado:* ${v.ago}\n`,   
+                    id: `${usedPrefix}yta ${v.url}`  
+                },  
+                {  
+                    header: "🎥 MP4",  
+                    title: "" ,  
+                    description: `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título:* ${v.title}\n▢ 📆 *Publicado:* ${v.ago}\n`,   
+                    id: `${usedPrefix}ytv ${v.url}`  
+                }  
+            ]  
+        });  
+    }  
+  
+    await conn.sendList(m.chat, '  ≡ *YT-SEARCH MUSIC*🔎', `\n 📀 Resultados de: *${text}*\n\nKanBot by Stiiven`, `Click Aquí`, ytres[0].image, listSections, m);  
+};  
+  
+handler.help = ['yts']  
+handler.tags = ['search']  
+handler.command = ['yts', 'ytsearch']   
+handler.disabled = false  
+handler.group = true  
+  
+export default handler;handler.command = ['yts', 'ytsearch'];
 handler.disabled = false;
 handler.group = true;
 
