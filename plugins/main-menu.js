ddee1194e4c343de6cd687c5935d4396a82461f4
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 19:20:14 -0500

    Add files via upload

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -27,26 +27,24 @@ let tags = {
 
 const defaultMenu = {
   before: `
-  *─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-“ Hola *%name* soy *Ai Hoshino*, %greeting ”
-
-╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*
-┊ ‹‹ *Hello* :: *%name*
-┊•*⁀➷ °⭒⭒⭒ *【 ✯ Starlights Team ✰ 】*
-╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩
-┊🍬 [ *Modo* :: *Público*
-┊📚 [ *Baileys* :: *Multi Device*
-┊⏱ [ *Tiempo Activo* :: *%muptime*
-┊👤 [ *Usuarios* :: *%totalreg*
-╰─────────
+◈ ━━━━━ *ᴋᴀɴʙᴏᴛ ┃ ⱽ²* ━━━━━ ◈
+ 
+👋🏻 Hola! *%name*
+👥 Usuarios : %totalreg
+🟢 Tiempo activo : %muptime
+%sbot
+▢  FOLLOW ME
+• https://whatsapp.com/channel/0029VakhAHc5fM5hgaQ8ed2N
+────────────
 %readmore
-*─ׄ─ׅ─ׄ─⭒ L I S T A  -  M E N Ú S ⭒─ׄ─ׅ─ׄ─*
+  ≡ *LISTA DE MENUS*
+
 `.trimStart(),
-  header: '╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊ ‹‹ *Category* :: *%category*\n┊•*⁀➷ °⭒⭒⭒ •*⁀➷ °⭒⭒⭒\n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩',
-    body: '│❄️⃟🎄┊%cmd %islimit %isPremium\n',
-   footer: '╰─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒\n',
-    after: `> [ ✰ ] ${textbot}`,
+  header: '┌─⊷ *%category*',
+  body: '▢ %cmd ',
+  footer: '└───────────\n',
+  after: `
+`,
 }
 
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
@@ -163,9 +161,13 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let pp14 = 'https://tinyurl.com/ymlqb6ml'
     let pp15 = 'https://tinyurl.com/ykv7g4zy'
     let img = `./storage/img/menu.jpg`
-    await m.react('⭐')
+    await m.react('⚡')
+    conn.sendButton(m.chat, text.trim(), `▢ 𝙺𝚊𝚗𝙱𝚘𝚝 ┃ ⱽ²\nʙʏ ꜱᴛɪɪᴠᴇɴ`, img, [
+      ['⏍ Info', `${_p}botinfo`],
+      ['⌬ Grupos', `${_p}grupos`]
+    ], m, )
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
-    await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
+   // await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
    //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
 
   } catch (e) {
@@ -176,8 +178,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 
 handler.help = ['menu']
 handler.tags = ['main']
-handler.command = ['menu', 'help', 'menú'] 
-handler.register = true 
+handler.command = ['menu', 'menú'] 
+handler.group = true
 export default handler
 
 
