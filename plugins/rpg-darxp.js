3cf89c1b2cc8baedad003d1f5bd67e5f30f6ac9a
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 21:46:24 -0500

    Update rpg-darxp.js

--- plugins/rpg-darxp.js
+++ plugins/rpg-darxp.js
@@ -6,15 +6,15 @@ let handler = async (m, { conn, text }) => {
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw '🚩 Menciona al usuario con *@user.*'
+  if (!who) throw '⚡ Menciona al usuario con *@user.*'
   let txt = text.replace('@' + who.split`@`[0], '').trim()
-  if (!txt) throw '🚩 Ingrese la cantidad de *💫 XP* que quiere transferir.'
+  if (!txt) throw '⚠️ Ingrese la cantidad de *💫 XP* que quiere transferir.'
   if (isNaN(txt)) throw 'Sólo números.'
   let xp = parseInt(txt)
   let exp = xp
   let imt = Math.ceil(xp * impuesto)
   exp += imt
-  if (exp < 1) throw '🚩 Mínimo es 1 💫 XP.*'
+  if (exp < 1) throw '✳️ Mínimo es 1 💫 XP.*'
   let users = global.db.data.users
   if (exp > users[m.sender].exp) throw '*💫 XP* insuficiente para transferir.'
   users[m.sender].exp -= exp
@@ -28,6 +28,6 @@ Total gastado: *${-exp} 💫 XP*`)
 handler.help = ['darxp *@user <cantidad>*']
 handler.tags = ['rpg']
 handler.command = ['darxp'] 
-handler.register = true 
+handler.group = true 
 
 export default handler
