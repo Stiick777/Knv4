b7470d810840a61f8949ae3b102826a523451092
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 22:30:22 -0500

    Update group-settings.js

--- plugins/group-settings.js
+++ plugins/group-settings.js
@@ -4,12 +4,12 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         'cerrar': 'announcement',
     }[(args[0] || '')]
     if (isClose === undefined)
-        return conn.reply(`🚩 Elija una opción.\n\n*${usedPrefix + command}* abrir\n${usedPrefix + command}* cerrar`, m, rcanal)
+        return conn.reply(`🔰 Elija una opción.\n\n*${usedPrefix + command}* abrir\n${usedPrefix + command}* cerrar`, m, rcanal)
     await conn.groupSettingUpdate(m.chat, isClose)
 }
 handler.help = ['group *abrir/cerrar*']
 handler.tags = ['group']
-handler.command = ['group', 'grupo'] 
+handler.command = ['group', 'grupo', 'gp'] 
 handler.admin = true
 handler.botAdmin = true
 
