1d3bd8a744ae44d42359191d576e341b096ee2fa
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 20:35:47 -0500

    Update tools-readviewonce.js

--- plugins/tools-readviewonce.js
+++ plugins/tools-readviewonce.js
@@ -19,6 +19,6 @@ handler.help = ['ver']
 handler.tags = ['tools']
 handler.command = ['readviewonce', 'read', 'ver', 'readvo'] 
 //handler.limit = 1
-handler.register = true 
+handler.group = true 
 
 export default handler
