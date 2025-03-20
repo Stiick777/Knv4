db9c955d32efb0574c7a4de2ec451f43b3f9e9b7
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 21:30:14 -0500

    Update rpg-retirar.js

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -2,12 +2,12 @@
 
 let handler = async (m, { args }) => {
    let user = global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *⭐ Estrellas* que deseas Retirar.')
+   if (!args[0]) return m.reply('🔥 Ingresa la cantidad de *⭐ Estrellas* que deseas Retirar.')
    if (args[0] == 'all') {
       let count = parseInt(user.bank)
       user.bank -= count * 1
       user.limit += count * 1
-      await m.reply(`🚩 Retiraste *${count} ⭐ Estrellas* del Banco.`)
+      await m.reply(`⚡ Retiraste *${count} ⭐ Estrellas* del Banco.`)
       return !0
    }
    if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.')
@@ -16,11 +16,11 @@ let handler = async (m, { args }) => {
    if (user.bank < count) return m.reply(`Solo tienes *${user.bank} ⭐ Estrellas* en el Banco.`)
    user.bank -= count * 1
    user.limit += count * 1
-   await m.reply(`🚩 Retiraste *${count} ⭐ Estrellas* del Banco.`)
+   await m.reply(`✳️ Retiraste *${count} ⭐ Estrellas* del Banco.`)
 }
 
 handler.help = ['retirar']
 handler.tags = ['rpg']
 handler.command = ['withdraw', 'retirar', 'wd']
-handler.register = true 
+handler.group = true 
 export default handler
