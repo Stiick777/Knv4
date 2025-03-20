97c0fa9631cba2aaf8f2c7fc58eecbf9b39c5715
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 20:20:34 -0500

    Update tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -1,44 +1,23 @@
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
-import fetch from 'node-fetch'
 
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, '💥 Responde a una *Imagen* o *Vídeo.*', m, rcanal)
-  await m.react('🕛')
-  try {
+  
+  if (!mime) throw '✳️ Responde a una imagen o video para subirlo.'
+
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
-  let img = await (await fetch(`${link}`)).buffer()
-  let txt = `乂  *L I N K - E N L A C E*  乂\n\n`
-      txt += `*» Enlace* : ${link}\n`
-      txt += `*» Acortado* : ${await shortUrl(link)}\n`
-      txt += `*» Tamaño* : ${formatBytes(media.length)}\n`
-      txt += `*» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
-      txt += `> *${dev}*`
 
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, fkontak, rcanal)
-await m.react('✅')
-} catch {
-await m.react('❌')
-}}
+  m.reply(`▢ *Tamaño:* ${(media.length / (1024 * 1024)).toFixed(2)} MB
+▢ ${isTele ? '(Sin fecha de caducidad)' : '(Desconocido)'} 
+▢ *URL:* ${link}`)
+}
+
 handler.help = ['tourl']
 handler.tags = ['tools']
-handler.command = ['tourl','upload']
+handler.command = ['upload', 'tourl']
+
 export default handler
-
-function formatBytes(bytes) {
-  if (bytes === 0) {
-    return '0 B';
-  }
-  const sizes = ['B', 'KB', 'MB', 'GB', 'TB']
-  const i = Math.floor(Math.log(bytes) / Math.log(1024))
-  return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
-}
-
-async function shortUrl(url) {
-        let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
-        return await res.text()
-}
