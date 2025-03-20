2651ffa45ed21a52c63dcefbb14350a7d8f1047e
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 20:59:03 -0500

    Update tools-acortar.js

--- plugins/tools-acortar.js
+++ plugins/tools-acortar.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
 let handler = async (m, {conn, args, text}) => {
 try {
-if (!text) return m.reply('🚩 Ingresa el enlace que deseas acortar.')
+if (!text) return m.reply('✳️ Ingresa el enlace que deseas acortar.')
 let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-if (!shortUrl1) return m.reply(`🚩 Ingresa el enlace que deseas acortar.`)
+if (!shortUrl1) return m.reply(`✳️ Ingresa el enlace que deseas acortar.`)
 let done = `*» Url Acortado* : ${shortUrl1}`
 m.reply(done)
 } catch { 
@@ -13,5 +13,5 @@ handler.help = ['acortar'].map((v) => v + ' *<url>*')
 handler.tags = ['tools']
 handler.command = /^(tinyurl|short|acortar|corto)$/i;
 handler.fail = null
-handler.register = true 
+handler.group = true 
 export default handler
