73de72a72d9e9f956dcf847bb2703fa297d994e9
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 21:06:16 -0500

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -12,7 +12,7 @@ resolve(json)
 reject(err)
 })})
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply(`🚩 Ejemplo: *${usedPrefix + command}* 😎+🤑`)
+if (!args[0]) return m.reply(`✳️ Ejemplo: *${usedPrefix + command}* 😎+🤑`)
 let [emoji, emoji2] = text.split`+`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
 for (let res of anu.results) {
@@ -23,6 +23,6 @@ handler.help = ['emojimix *<emoji+emoji>*']
 handler.tags = ['sticker']
 handler.command = ['emojimix'] 
 //handler.limit = 1
-handler.register = true 
+handler.group = true 
 
 export default handler
