4c60ec3608e9e311ffa0e661d5c84e729284a06d
Stiick777 <kanixxxcexx@gmail.com>
2025-03-20 01:02:09 -0500

    Update youtube-ytsearch.js

--- plugins/youtube-ytsearch.js
+++ plugins/youtube-ytsearch.js
@@ -1,13 +1,12 @@
-
 import yts from 'yt-search';
+
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-    if (!text) throw `✳️ ${mssg.example} *${usedPrefix + command}* Lil Peep hate my life`;
+    if (!text) throw `✳️ Ejemplo: *${usedPrefix + command}* Lil Peep hate my life`;
     m.react('📀');
     
     let result = await yts(text);
     let ytres = result.videos;
     
-
     let listSections = [];
     for (let index in ytres) {
         let v = ytres[index];
@@ -17,20 +16,20 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
                 {
                     header: '🎶 MP3',
                     title: "",
-                    description: `▢ ⌚ *${mssg.duration}:* ${v.timestamp}\n▢ 👀 *${mssg.views}:* ${v.views}\n▢ 📌 *${mssg.title}* : ${v.title}\n▢ 📆 *${mssg.aploud}:* ${v.ago}\n`, 
+                    description: `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título:* ${v.title}\n▢ 📆 *Publicado:* ${v.ago}\n`, 
                     id: `${usedPrefix}yta ${v.url}`
                 },
                 {
                     header: "🎥 MP4",
                     title: "" ,
-                    description: `▢ ⌚ *${mssg.duration}:* ${v.timestamp}\n▢ 👀 *${mssg.views}:* ${v.views}\n▢ 📌 *${mssg.title}* : ${v.title}\n▢ 📆 *${mssg.aploud}:* ${v.ago}\n`, 
+                    description: `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título:* ${v.title}\n▢ 📆 *Publicado:* ${v.ago}\n`, 
                     id: `${usedPrefix}ytv ${v.url}`
                 }
             ]
         });
     }
 
-    await conn.sendList(m.chat, '  ≡ *YT-SEARCH MUSIC*🔎', `\n 📀 Resultados de: *${text}*\n\nKanBot by Stiiven`, `Click Aqui`, ytres[0].image, listSections, m);
+    await conn.sendList(m.chat, '  ≡ *YT-SEARCH MUSIC*🔎', `\n 📀 Resultados de: *${text}*\n\nKanBot by Stiiven`, `Click Aquí`, ytres[0].image, listSections, m);
 };
 
 handler.help = ['yts']
@@ -39,4 +38,4 @@ handler.command = ['yts', 'ytsearch']
 handler.disabled = false
 handler.group = true
 
-export default handler
+export default handler;
