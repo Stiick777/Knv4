f0dcc04a03458f12b2e219c89cf6bcd197854628
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 23:00:04 -0500

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,30 +1,25 @@
-import translate from '@vitalets/google-translate-api'
-import fetch from 'node-fetch'
-const handler = async (m, {text, command, args, usedPrefix}) => {
-  if (!text) return m.reply(`*• Ingresa un texto*\n\n*Ejemplo:*\n*${usedPrefix + command}* Hola bot`)
-  try {
-    const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=es');
-    const resSimi = await api.json();
-    m.reply(resSimi.success);
-  } catch {
-    try {
-      if (text.includes('Hola')) text = text.replace('Hola', 'Hello');
-      if (text.includes('hola')) text = text.replace('hola', 'Hello');
-      if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO');
-      const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
-      const resu = await reis.json();
-      const nama = m.pushName || '1';
-      const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
-      const res = await api.json();
-      const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
-      const resu2 = await reis2.json();
-      m.reply(resu2[0][0][0]);
-    } catch {
-      throw `*Miku Bot😺* | 「 *ERROR* 」\n\nOcurrió un *Error*`;
-    }
-  }
-};
-handler.help = ['simi']
-handler.tags = ['fun'];
-handler.command = /^((sim)?simi|alexa|cortana|bot)$/i;
-export default handler;
+import fetch from 'node-fetch'  
+let handler = async (m, { conn, text, usedPrefix, command }) => {  
+
+  let lang = global.db.data.users[m.sender].language  
+  if (!text) throw '✳️ Debes escribir un mensaje para que el bot responda.'  
+  m.react('🗣️')   
+
+  try {   
+    let res = await fetch('https://api.simsimi.vn/v1/simtalk', {  
+      method: 'POST',  
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },  
+      body: `text=${encodeURIComponent(text)}&lc=${lang}&key=`  
+    })  
+    let json = await res.json()  
+    m.reply(json.message.replace(/simsimi/gi, botName), null, fwc)  
+  } catch {  
+    m.reply('❎ Intenta de nuevo más tarde. La API de SimSimi no está disponible.')  
+  }  
+
+}  
+handler.help = ['bot']  
+handler.tags = ['game']  
+handler.command = ['bot', 'simi']   
+handler.group = true  
+export default handler
