acf467decf4a9edf1c72e34feb717409636efe49
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 20:38:20 -0500

    Update tools-readviewonce.js

--- plugins/tools-readviewonce.js
+++ plugins/tools-readviewonce.js
@@ -1,24 +1,16 @@
-let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
-if (!m.quoted) return conn.reply(m.chat, `🚩 Responde a una imagen ViewOnce.`, m)
-if (m.quoted.mtype !== 'viewOnceMessageV2') return conn.reply(m.chat, `🚩 Responde a una imagen ViewOnce.`, m)
-let msg = m.quoted.message
-let type = Object.keys(msg)[0]
-let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
-let buffer = Buffer.from([])
-for await (const chunk of media) {
-buffer = Buffer.concat([buffer, chunk])
+    let q = m.quoted ? m.quoted : m
+    try {
+        let media = await q.download?.()
+        let caption = q.text || ''
+        await conn.sendFile(m.chat, media, null, caption, m, null, fwc)
+    } catch (e) {
+        m.reply('✳️ Eso no es un mensaje de View Once.')
+    }
 }
-if (/video/.test(type)) {
-return conn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m)
-} else if (/image/.test(type)) {
-return conn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
-}}
-handler.help = ['ver']
+handler.help = ['readvo']
 handler.tags = ['tools']
 handler.command = ['readviewonce', 'read', 'ver', 'readvo'] 
-//handler.limit = 1
 handler.group = true 
-
 export default handler
