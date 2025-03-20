622c18e76e946251599b5c00b85da2e8257a6a90
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 20:04:40 -0500

    Update tools-tovid.js

--- plugins/tools-tovid.js
+++ plugins/tools-tovid.js
@@ -1,9 +1,9 @@
 import { webp2mp4 } from '../lib/webp2mp4.js'
 import { ffmpeg } from '../lib/converter.js'
 let handler = async (m, { conn }) => {
-if (!m.quoted) return conn.reply(m.chat, '🚩 Responde a un *Sticker Animado.*', m, rcanal)
+if (!m.quoted) return conn.reply(m.chat, '✳️ Responde a un *Sticker Animado.*', m, rcanal)
 let mime = m.quoted.mimetype || ''
-if (!/webp|audio/.test(mime)) return conn.reply(m.chat, '🚩 Responde a un *Sticker Animado.*', m, rcanal)
+if (!/webp|audio/.test(mime)) return conn.reply(m.chat, '✳️ Responde a un *Sticker Animado.*', m, rcanal)
 await m.react('🕓')
 try {
 let media = await m.quoted.download()
@@ -27,6 +27,6 @@ await m.react('✖️')
 handler.help = ['tovid *<sticker>*']
 handler.tags = ['sticker', 'tools']
 handler.command = ['tovideo', 'tovid']
-handler.register = true 
+handler.group = true
 
 export default handler
