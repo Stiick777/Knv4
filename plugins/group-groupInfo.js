aa3c16309de10103bf076a521d2ea91275244f77
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 22:41:16 -0500

    Update group-groupInfo.js

--- plugins/group-groupInfo.js
+++ plugins/group-groupInfo.js
@@ -1,41 +1,42 @@
-//
-
-let handler = async (m, { conn, participants, groupMetadata }) => {
-    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
-    const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, delete: del } = global.db.data.chats[m.chat]
-    const groupAdmins = participants.filter(p => p.admin)
-    const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
-    const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
-    let text = `
-╭─「 *INFO DE GRUPO* 」
-║❥ *ID:* ${groupMetadata.id}
-║❥ *Nombre:* ${groupMetadata.subject}
-║❥ *Miembros:* ${participants.length}
-║❥ *Dueño de Grupo:* @${owner.split('@')[0]}
-║❥ *Admins:* 
+const handler = async (m, {conn, participants, groupMetadata}) => {
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || `${global.icons}`;
+  const {antiToxic, reaction, antiTraba, antidelete, antiviewonce, welcome, detect, antiLink, antiLink2, modohorny, autosticker, audios} = global.db.data.chats[m.chat];
+  const groupAdmins = participants.filter((p) => p.admin);
+  const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
+  const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
+  const text = `💥 *INFO GRUPO*
+💌 *ID:*
+→ ${groupMetadata.id}
+🥷 *Nombre:*
+→ ${groupMetadata.subject}
+🌟 *Descripción:*
+→ Leelo puta (￣へ ￣ 凸
+💫 *Miembros:*
+→ ${participants.length} Participantes
+👑 *Creador del Grupo:*
+→ @${owner.split('@')[0]}
+🏆 *Administradores:*
 ${listAdmin}
-║❥ *Configuración de grupo:*
-║❥ • ${isBanned ? '✅' : '❎'} Baneado
-║❥ • ${welcome ? '✅' : '❎'} Bienvenida
-║❥ • ${detect ? '✅' : '❎'} Detector
-║❥ • ${del ? '❎' : '✅'} Anti Delete
-║❥ • ${antiLink ? '✅' : '❎'} Anti Link WhatsApp
-╰────
-*Configuración de mensajes:*
-• Bienvenida: ${sWelcome}
-• Despedida: ${sBye}
-• Promovidos: ${sPromote}
-• Degradados: ${sDemote}
 
-*Descripción* :
-• ${groupMetadata.desc?.toString() || 'desconocido'}
-`.trim()
-    conn.sendFile(m.chat, pp, 'pp.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
-}
+💭 *CONFIGURACIÓN*
 
-handler.help = ['infogp']
-handler.tags = ['group']
-handler.command = ['infogrupo', 'groupinfo', 'infogp'] 
-handler.group = true
-
-export default handler
+◈ *Welcome:* ${welcome ? '✅' : '❌'}
+◈ *Detect:* ${detect ? '✅' : '❌'}  
+◈ *Antilink:* ${antiLink ? '✅' : '❌'} 
+◈ *Antilink 𝟸:* ${antiLink2 ? '✅' : '❌'} 
+◈ *Modohorny:* ${modohorny ? '✅' : '❌'} 
+◈ *Autosticker:* ${autosticker ? '✅' : '❌'} 
+◈ *Audios:* ${audios ? '✅' : '❌'} 
+◈ *Antiver:* ${antiviewonce ? '✅' : '❌'} 
+◈ *Reacción* ${reaction ? "✅️" : "❌️"}
+◈ *Delete:* ${antidelete ? '✅' : '❌'} 
+◈ *Antitoxic:* ${antiToxic ? '✅' : '❌'} 
+◈ *Antitraba:* ${antiTraba ? '✅' : '❌'} 
+`.trim();
+  conn.sendFile(m.chat, pp, 'img.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
+};
+handler.help = ['infogrupo'];
+handler.tags = ['group'];
+handler.command = ['infogrupo', 'infgp'];
+handler.group = true;
+export default handler;
