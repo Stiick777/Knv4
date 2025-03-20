16df3b7178091fb3fdd87e8d91e1ffb0fc1ad270
Stiick777 <kanixxxcexx@gmail.com>
2025-03-19 23:02:55 -0500

    Update fun-pregunta.js

--- plugins/fun-pregunta.js
+++ plugins/fun-pregunta.js
@@ -1,12 +1,38 @@
-let handler = async (m, { command, text }) => m.reply(`
-*⁉️ 𝐏𝐑𝐄𝐆𝐔𝐍𝐓𝐀𝐒 ⁉️*
-  
-*𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰:* ${text}
-*𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible'].getRandom()}
-`.trim(), null, m.mentionedJid ? {
-mentions: m.mentionedJid
-} : {})
-handler.help = ['pregunta *<texto>*']
-handler.tags = ['fun']
-handler.command = /^pregunta|preguntas|apakah$/i
-export default handler
+var handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) return conn.reply(m.chat, `💥 *Ingrese un texto a preguntar*\n\n💣 Ejemplo: ${usedPrefix + command} ¿Hoy estallaremos algo?`, m);
+
+    const username = '@' + m.sender.split('@')[0]; // Obtiene el username del usuario que envió el mensaje
+    await m.react('❔');
+    await delay(1000 * 1);
+    await m.react('❓');
+    await delay(1000 * 1);
+    await m.react('❔');
+    await delay(1000 * 1);
+
+    let res = [
+        'Si', 
+        'Tal vez sí', 
+        'Posiblemente', 
+        'Probablemente no', 
+        'No', 
+        'Imposible', 
+        'tal vez no',
+        'quizas',
+        'para nada',
+        'siempre',
+        'Por eso te dejo tu ex', 
+        'No te dire la respuesta', 
+        'quien sabe' 
+    ].getRandom();
+
+    await conn.reply(m.chat, `👤 ${username}\n\n• *Pregunta:* ${text}\n• *Respuesta:* ${res}`, m);
+};
+
+handler.help = ['pregunta'];
+handler.tags = ['game'];
+handler.command = ['pregunta', 'preguntas'];
+handler.group = true;
+
+export default handler;
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
